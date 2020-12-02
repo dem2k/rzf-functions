f9f508342d1171e577210d749c7ee1bd7b4949d0
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2020-12-02 13:13:17 +0100

    error handling.

--- next-ginster-file.ps1
+++ next-ginster-file.ps1
@@ -3,14 +3,15 @@ function Set-NextGinsterXmlFile {
         [Parameter(Mandatory)]
         $GinsterXml
     )
-    $fileItem = Get-Item $GinsterXml
+    $fileItem = Get-Item $GinsterXml -ErrorAction Stop
     $prefix = ($fileItem.basename -split "_\d+")[0]
     $sufix = [int]($fileItem.basename -split "_")[-1] + 1
     $outFile = "{0}_{1:d5}.xml" -f $prefix, $sufix
     $timeNow = "{0:yyyyMMddHHmmss}" -f $(Get-Date)
     $encodingIso885915 = [System.Text.Encoding]::GetEncoding('ISO-8859-15')
-    $writer = New-Object System.IO.StreamWriter($OutFile, $false, $encodingIso885915)
     $xml = [xml](Get-Content -Encoding $encodingIso885915 $GinsterXml)
+    if(-not $xml) {throw "wrong xml"}
+    $writer = New-Object System.IO.StreamWriter($OutFile, $false, $encodingIso885915)
     $xml.GINSTER.Request.SetAttribute("lfdLieferung", $sufix)
     $xml.GINSTER.Request.UpdateList.Update.AnwInfo.SetAttribute("technBearbDat", $timeNow)
     $xml.Save($writer)
