8349267a5ce5c1a8ff768fe5782194b361df5b92
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2020-12-02 12:57:30 +0100

    Split on \d+

--- next-ginster-file.ps1
+++ next-ginster-file.ps1
@@ -4,9 +4,9 @@ function Set-NextGinsterXmlFile {
         $GinsterXml
     )
     $fileItem = Get-Item $GinsterXml
-    $prefix = ($fileItem -split "_")[0]
+    $prefix = ($fileItem.basename -split "_\d+")[0]
     $sufix = [int]($fileItem.basename -split "_")[-1] + 1
-    $outFile = "{0}_NW_{1:d5}.xml" -f $prefix, $sufix
+    $outFile = "{0}_{1:d5}.xml" -f $prefix, $sufix
     $timeNow = "{0:yyyyMMddHHmmss}" -f $(Get-Date)
     $encodingIso885915 = [System.Text.Encoding]::GetEncoding('ISO-8859-15')
     $writer = New-Object System.IO.StreamWriter($OutFile, $false, $encodingIso885915)
@@ -15,5 +15,6 @@ function Set-NextGinsterXmlFile {
     $xml.GINSTER.Request.UpdateList.Update.AnwInfo.SetAttribute("technBearbDat", $timeNow)
     $xml.Save($writer)
     $writer.Close()
-    Move-Item $GinsterXml $env:TEMP
+    Move-Item $GinsterXml $env:TEMP -Force
+    return "$outfile"
 }
