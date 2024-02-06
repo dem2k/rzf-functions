873cf08453853f10b8c2b5fae8e49d12ced32575
x004123 <dimitri.kleyn@fv.nrw.de>
2024-02-06 09:53:53 +0100

    Add Function Get-GinsterXmlStnrTyp.

--- get-ginster-xml-attributes.ps1
+++ get-ginster-xml-attributes.ps1
@@ -28,3 +28,21 @@ function Get-GinsterXmlStnr {
     $xml = Select-Xml @SelectXmlParams
     return $xml.Node."#text"
 }
+
+function Get-GinsterXmlStnrTyp {
+    param ([parameter(Mandatory = $true)] $GinsterXml )
+    $ns = Get-GinsterXmlNamespace $GinsterXml
+    $SelectStnr = @{
+        Path      = $GinsterXml
+        XPath     = '//ns:Steuerkonto/@stNr'
+        Namespace = @{ ns = $ns }
+    }
+    $xmlStnr = Select-Xml @SelectStnr
+    $SelectTyp = @{
+        Path      = $GinsterXml
+        XPath     = '//ns:Steuerkonto/@typ'
+        Namespace = @{ ns = $ns }
+    }
+    $xmlTyp = Select-Xml @SelectTyp
+    return ("{0}-{1}" -f ([string]$xmlStnr.Node."#text"), ([string]$xmlTyp.Node."#text"))
+}
